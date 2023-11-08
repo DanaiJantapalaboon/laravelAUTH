a7a6edfb75eeb84df038c01d885333d0a1cda7a6
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-11-08 16:22:30 +0700

    update 8NOV23

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid" style="border-bottom: 2px solid red">
+        <a class="navbar-brand" href="#"><a class="navbar-brand" href="#"><img src="{{ asset('storage/' . $companyLogo) }}" alt="{{ $companyName }}" style="width: 150px;"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Fedatures</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,30 +6,7 @@
 </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid" style="border-bottom: 2px solid red">
-            <a class="navbar-brand" href="#"><a class="navbar-brand" href="#"><img src="{{ asset('storage/' . $companyLogo) }}" alt="{{ $companyName }}" style="width: 150px;"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Fedatures</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    @include('includes.navbar')
 
     <button class="btn btn-primary">AMD Athlon</button>
     <button class="btn btn-secondary">AMD Athlon</button>
@@ -43,7 +20,7 @@
     <p class="text-primary">AMD Athlon</p>
 
 
-    <div>
+    {{-- <div>
         @if (Route::has('login'))
             <div>
                 @auth
@@ -57,11 +34,10 @@
                 @endauth
             </div>
         @endif
+    </div> --}}
 
-
-        <div>
-            Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-        </div>
+    <div>
+        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
     </div>
     
 </body>
