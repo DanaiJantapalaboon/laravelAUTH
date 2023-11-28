cd09a8fee92048e27604f8e6be5277ffba49b240
Danai Jantapalaboon <danai.athlon@gmail.com>
2023-11-28 10:03:55 +0700

    update_userrole_28NOV23

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('lastname', 100);
             $table->string('position', 100);
             $table->string('email')->unique();
-            $table->string('role', 50);
+            $table->string('role', 50)->default("Admin");
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             'lastname' => 'Jantapalaboon',
             'position' => 'FullStack Web Developer',
             'email' => 'danai.athlon@gmail.com',
-            'role' => 'admin',
+            'role' => 'Admin',
             'password' => Hash::make(1234),
             'created_at' => now(),
             'updated_at' => now()
